4a97c787fd71616dddcea7e9d26e0fffa009f462
Joshua ben Jore <jjore@cpan.org>
2010-06-11 08:43:56 +0000

    version 2.16 (commit generated from cpan)
    Manually add VERSION to Data::Dump::Streamer::Deparser to make CPAN
    indexer happy.

--- lib/Data/Dump/Streamer.pm
+++ lib/Data/Dump/Streamer.pm
@@ -34,7 +34,7 @@ BEGIN{ $HasPadWalker=eval "use PadWalker 0.99; 1"; }
 
 BEGIN {
     #$Id: Streamer.pm 40 2007-12-22 00:37:55Z demerphq $#
-    $VERSION   ='2.15';
+    $VERSION   ='2.16';
     $VERSION = eval $VERSION; # used for beta stuff.
     @ISA       = qw(Exporter DynaLoader);
     @EXPORT=qw(Dump DumpLex DumpVars);
@@ -3668,6 +3668,11 @@ package Data::Dump::Streamer::Deparser;
 use B::Deparse;
 our @ISA=qw(B::Deparse);
 my %cache;
+
+our $VERSION = '2.16';
+if ( $VERSION ne $Data::Dump::Streamer::VERSION ) {
+    die "Incompatible Data::Dump::Streamer::Deparser v$VERSION vs Data::Dump::Streamer v$Data::Dump::Streamer::VERSION";
+}
 
 sub dds_usenames {
     my $self=shift;
